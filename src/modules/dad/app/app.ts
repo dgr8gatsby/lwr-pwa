0b1ffa6b5b0951a31120a25d77d000ab3897cf6e
Kevin <kevinhill@live.com>
2021-01-26 08:06:49 -0800

    Addng in graphics for pwa

--- src/modules/dad/app/app.ts
+++ src/modules/dad/app/app.ts
@@ -1,3 +1,98 @@
-import {LightningElement} from 'lwc';
+import { LightningElement } from 'lwc';
+import Navigo from 'navigo';
 
-export default class App extends LightningElement {}
+const router = new Navigo('/', { hash: true });
+
+export default class App extends LightningElement {
+    constructor() {
+        super();
+
+        router.on('/', async () => { });
+        // Get the correct joke when navigating to this route
+        router.on('/joke/:id', async (url: any) => {
+            await this.getJokeById(url.data.id);
+        })
+        this.initialize();
+    }
+
+    jokeIds = [];
+    currentJokeIndex: number = 0;
+    currentJoke: any = null;
+
+    // Async calls to the backend API
+    async getJokes() {
+        let response: any = await fetch('http://localhost:3001/api/jokes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(this.handleErrors)
+            .catch((error) => {
+                console.error(`Error: ${error}`);
+            });
+
+        let jokeIds = await response.json();
+        this.jokeIds = Object.values(jokeIds);
+    }
+
+    async getJokeById(id: string) {
+        let joke = await fetch(`http://localhost:3001/api/joke/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        this.currentJoke = await joke.json();
+    }
+
+    // Getters/Setters
+    get joke() {
+        if (this.currentJoke !== null && this.currentJoke !== undefined) {
+            return this.currentJoke;
+        } else {
+            return null;
+        }
+    }
+
+    // Methods
+    async initialize() {
+        await this.getJokes();
+        var route = router.getCurrentLocation();
+        if (route.url.length) {
+            // Try to navigate to the existing joke if one was pasted in
+            const path = window.location.hash.split('/');
+            if (path.length === 3 && path[1] === 'joke' && path[2].length >= 24) {
+                router.navigate(`joke/${path[2]}`);
+            } else {
+                this.next();
+            }
+        } else {
+            this.next();
+        }
+    }
+
+    next() {
+        if (this.currentJokeIndex < this.jokeIds.length - 1) {
+            this.currentJokeIndex++;
+        } else {
+            this.currentJokeIndex = 0;
+        }
+        router.navigate(`joke/${this.jokeIds[this.currentJokeIndex]}`);
+    }
+
+    prev() {
+        if (this.currentJokeIndex > 1) {
+            this.currentJokeIndex--;
+        } else {
+            this.currentJokeIndex = this.jokeIds.length - 1;
+        }
+        router.navigate(`joke/${this.jokeIds[this.currentJokeIndex]}`);
+    }
+
+    handleErrors(response: any) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
+    }
+}
---
+++ src/serviceWorkers/sw.js
@@ -0,0 +1,39 @@
+const cacheName = 'staledadjokes_v4';
+
+const APPFILES = [
+  '/index.html',
+  '/public/assets/favicon.ico',
+  '/public/assets/mustache.svg',
+];
+
+self.addEventListener ('install', e => {
+  console.log ('[Service Worker] Install');
+  e.waitUntil (
+    caches.open (cacheName).then (cache => {
+      console.log ('[Service Worker] Caching all: app shell and content');
+      console.log (cache);
+      console.log (APPFILES);
+      return cache.addAll (APPFILES);
+    })
+  );
+});
+
+self.addEventListener ('fetch', e => {
+  e.respondWith (
+    caches.match (e.request).then (r => {
+      console.log ('[Service Worker] Fetching resource: ' + e.request.url);
+      return (
+        r ||
+        fetch (e.request).then (response => {
+          return caches.open (cacheName).then (cache => {
+            console.log (
+              '[Service Worker] Caching new resource: ' + e.request.url
+            );
+            cache.put (e.request, response.clone ());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
