9cabd0d7027c4a94b084a10ede076b0ea4b10421
Kevin Venkiteswaran <kvenkiteswaran@salesforce.com>
2021-02-07 09:51:24 -0800

    Add wire adapter

---
+++ src/modules/dad/wires/wires.ts
@@ -0,0 +1,17 @@
+import { luvio } from './luvio';
+import { createWireAdapterConstructor } from '@luvio/lwc-luvio';
+
+
+import { getJokesAdapterFactory } from '../../../raml/generated/adapters/getJokes';
+import { getJokeAdapterFactory } from '../../../raml/generated/adapters/getJoke';
+
+// Create the luvio adapters
+const getJokesLuvioAdapter = getJokesAdapterFactory(luvio);
+const getJokeLuvioAdapter = getJokeAdapterFactory(luvio); 
+
+// Create wire adapters
+const GetJokesWireAdapter = createWireAdapterConstructor(getJokesLuvioAdapter, 'getJokes', luvio);
+const GetJokeWireAdapter = createWireAdapterConstructor(getJokeLuvioAdapter, 'getJoke', luvio);
+
+export GetJokesWireAdapter as getJokes;
+export GetJokeWireAdapter as getJoke;
