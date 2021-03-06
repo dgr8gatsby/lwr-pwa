a90a2ba984f768f46e3b5d569fcbe2d83f6c9135
gr8gatsby <kevinhill@live.com>
2021-03-05 22:03:52 -0800

    back to hash based routing

--- src/modules/dad/joke/joke.ts
+++ src/modules/dad/joke/joke.ts
@@ -31,7 +31,6 @@ export default class Joke extends LightningElement {
     }
 
     get tweetLink() {
-        console.log(window.location.href);
         return `${TWITTER_URL}?text=${encodeURI(this.joke.headline)}%0A${encodeURI(this.joke.punchline)}%0A&url=${encodeURI(window.location.href)}&hashtags=papajoke,dadabase`;
     }
 
