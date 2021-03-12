0a06393c5b0318713c5c3eb765e76a744b6b4b40
gr8gatsby <kevinhill@live.com>
2021-03-11 22:14:25 -0800

    updating error messages

--- src/serviceWorkers/sw.js
+++ src/serviceWorkers/sw.js
@@ -1,37 +1,54 @@
-const cacheName = 'staledadjokes_v16';
+const CACHE = "pwabuilder-precache";
 
-const APPFILES = [
-  //'./public/main.html',
-  //'./assets/favicon.ico',
-  './assets/mustache.svg',
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        return cache.addAll(APPFILES);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  );
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => {
-      console.log('[Service Worker] Fetching resource: ' + e.request.url);
-      return (
-        r ||
-        fetch(e.request).then(response => {
-          return caches.open(cacheName).then(cache => {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        })
-      );
-    })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: CACHE
+  })
+);
+// const cacheName = 'staledadjokes_v17';
+
+// const APPFILES = [
+//   //'./public/main.html',
+//   './assets/favicon.ico',
+//   './assets/mustache.svg',
+// ];
+
+// self.addEventListener('install', e => {
+//   e.waitUntil(
+//     caches
+//       .open(cacheName)
+//       .then(cache => {
+//         return cache.addAll(APPFILES);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//   );
+// });
+
+// self.addEventListener('fetch', e => {
+//   e.respondWith(
+//     caches.match(e.request).then(r => {
+//       console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//       return (
+//         r ||
+//         fetch(e.request).then(response => {
+//           return caches.open(cacheName).then(cache => {
+//             cache.put(e.request, response.clone());
+//             response.status = 200
+//             return response;
+//           });
+//         })
+//       );
+//     })
+//   );
+// });
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "src/assets/",
+  "globPatterns": [
+    "**/*.{png,svg,ico,json,html,css,ts,js}"
+  ],
+  "swDest": "src/serviceWorkers/sw.js"
+};
