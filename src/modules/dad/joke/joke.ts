dcd1dd08956fb8eb9e1d4bb5761c0961dc6b2bc6
Kevin Hill <kevinhill@live.com>
2021-03-19 07:35:40 -0700

    updaging joke

--- src/modules/dad/joke/joke.ts
+++ src/modules/dad/joke/joke.ts
@@ -26,10 +26,6 @@ export default class Joke extends LightningElement {
         return undefined;
     }
 
-    get nextJokeHref(){
-        return `#/jokes/${this.nextJoke}`;
-    }
-
     get tweetLink() {
         return `${TWITTER_URL}?text=${encodeURI(this.joke.headline)}%0A${encodeURI(this.joke.punchline)}%0A&url=${encodeURI(`https://www.papajoke.com/api/social/${this.joke._id}`)}&hashtags=papajoke,dadabase`;
     }
